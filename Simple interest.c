7d483d91d79693be4950deedd22445f9a8d71474
Neenu807 <62111901+Neenu807@users.noreply.github.com>
2020-03-13 10:21:41 +0530

    Update Simple interest.c

--- Simple interest.c
+++ Simple interest.c
@@ -1,2 +1,11 @@
-# cp
-Assignment-1
+/* program to find the simple interest */
+
+#include<stdio.h>
+void main()
+  {
+        float p,n,r,si ;
+        printf("enter the principle,no of years and rate\n");
+        scanf("%f%f%f",&p,&n,&r);
+        si=p*n*r/100;
+        printf(" the simple interest is=%f",si);
+  }
